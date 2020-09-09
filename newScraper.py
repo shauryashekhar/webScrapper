f06ad5f7311659c3557baac5868952f9a9cad6ec
Shaurya <shauryashekhar18@gmail.com>
2020-09-09 15:48:34 -0500

    Initial commit of working newScraper

---
+++ newScraper.py
@@ -0,0 +1,16 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+import csv
+from collections import defaultdict
+import json
+import sqlite3
+from sqlite3 import Error
+from queue import Queue 
+import sys
+import dataset
+
+if __name__ == "__main__":
+
+    print("Hello! This is the new script which uses the dataset package")
+    sys.exit(0)
